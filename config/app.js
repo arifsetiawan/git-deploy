936250615f9bea4fa1c4158790893514ed921825
Nurul Arif Setiawan <n.arif.setiawan@gmail.com>
2014-09-05 16:25:48 +0700

    fix config

--- config/app.js
+++ config/app.js
@@ -1,7 +1,7 @@
 
 var localRepo = {
   'dummy' : {
-    path : process.env.PORT ? 'C:/Users/ArifSetiawan/Repository/Simaya/dummyd' : '/home/simaya/apps/dummy',
+    path : process.env.PORT ? '/home/simaya/apps/dummy' : 'C:/Users/ArifSetiawan/Repository/Simaya/dummyd',
     branch : ['master', 'test']
   }
 }
@@ -27,5 +27,5 @@ module.exports = {
   port : process.env.PORT ? process.env.PORT : 3001,
   result : result,
   repo : process.env.PORT ? deployRepo : localRepo,
-  appDir : process.env.PORT ? process.cwd() : '/home/simaya/apps/deploy'
+  appDir : process.env.PORT ? '/home/simaya/apps/deploy' : process.cwd()
 }
